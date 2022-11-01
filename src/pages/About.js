c596508230a0e447099dd37c618ce1741bba2dfa
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-11-01 07:48:21 +0100

    Add singlePage component

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link, Route } from 'react-router-dom';
+import SinglePage from './SinglePage';
 
-const About = () => (
-  <div>About</div>
-);
+const About = () => {
+  const { url, path } = useRouteMatch();
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to={`${url}/about-app`}>About App</Link>
+        </li>
+        <li>
+          <Link to={`${url}/about-author`}>About Author</Link>
+        </li>
+      </ul>
+      <Route path={`${path}/:slug`}>
+        <SinglePage />
+      </Route>
+    </div>
+  );
+};
 
 export default About;
